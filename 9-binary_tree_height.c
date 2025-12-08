1e66bd0100bd8bb9203d22e656f9cd196f697833
Florian <12160@holbertonstudents.com>
2025-12-08 15:36:39 +0100

    fix: reorder variable declarations for clarity in binary_tree_height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,10 +8,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height;
+	size_t right_height;
+
 	if (tree == NULL)
 		return (0);
-
-	size_t left_height, right_height;
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
