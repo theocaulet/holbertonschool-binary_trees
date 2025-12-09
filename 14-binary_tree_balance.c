1ed81ded55b2b2014b9bcc90cd1922e036284242
theocaulet <caulet.theo@gmail.com>
2025-12-09 10:26:41 +0100

    fix: add function binary_tree_height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,3 +17,26 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (left_height - right_height);
 }
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: height of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+	return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+	return (left_height + 1);
+	else
+	return (right_height + 1);
+}
+
