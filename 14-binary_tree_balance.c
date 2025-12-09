0a65dbec31d42649db023d6e34ed7ed5225de6fa
theocaulet <caulet.theo@gmail.com>
2025-12-09 10:32:29 +0100

    fix: modify function for betty style

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,7 +11,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 	return (0);
-	
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
